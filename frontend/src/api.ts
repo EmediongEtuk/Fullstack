094a18f3371bcb68b51efcab4a10a67cb79cbef8
Emediong Etuk <64887490+EmediongEtuk@users.noreply.github.com>
2025-09-11 14:24:11 +0100

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Trip } from "./types";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+ baseURL: "https://fullstackbackend-nddx.onrender.com/api",
 });
 
 export const createTrip = (data: Partial<Trip>) => API.post("/trips/", data);
